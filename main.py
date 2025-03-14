92d351d0fdb4b78b8cf16d84c3d072d7267df258
Melvin Sim <96096344+melvinsim93@users.noreply.github.com>
2025-03-14 13:40:03 +0800

    Changes to be committed: 	modified:   main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello I am Melvin's python module copied from Anil Bidari, this is Version 3!\n"
+    return "Hello I am Melvin's python module copied from Anil Bidari, this is for the actual Assessment!\n"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
